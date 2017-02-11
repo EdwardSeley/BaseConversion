43b8ff129f07607000221b8985b02a08e45cd5f9
EdwardSeley <edwardseley@gmail.com>
2017-02-10 20:18:56 -0600

    Now able to convert to base 16

--- src/main/java/com/example/edward237/baseconversion/ConversionActivity.java
+++ src/main/java/com/example/edward237/baseconversion/ConversionActivity.java
@@ -43,45 +43,64 @@ public class ConversionActivity extends AppCompatActivity {
                 else {
                     result = fromBaseTen(inputNum, startBaseNum, endBaseNum, inputLength);
                 }
-
-                Toast.makeText(ConversionActivity.this, String.valueOf(result), Toast.LENGTH_SHORT).show();
+                
+		if (endBaseNum == 16 && !finalHexLetters.isEmpty())
+        		Toast.makeText(ConversionActivity.this, String.valueOf(finalHexLetters), Toast.LENGTH_SHORT).show();
+        	else
+                	JOptiToast.makeText(ConversionActivity.this, String.valueOf(result), Toast.LENGTH_SHORT).show();
             }
         });
 
     }
 
     public int fromBaseTen(int inputNum, int startBaseNum, int endBaseNum, int inputLength) {
-        int incrementNum = 0;
+     int incrementNum = 0;
         int result = 0;
         int finalValue = 0;
+        int inputNum = Integer.parseInt(inputValue);
         do {
             result = inputNum / ((int) Math.pow(endBaseNum, incrementNum));
             incrementNum++;
         }
         while (result != 0);
+        
         int decrementNum = incrementNum - 2;
-
-
+        String [] lettersArray = {"0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "A", "B", "C", "D", "E", "F"};
+        String hexLetters = "";
         while (decrementNum > -1) {
-
+        	
             result = inputNum / ((int) Math.pow(endBaseNum, decrementNum));
+            hexLetters = hexLetters.concat(String.valueOf(lettersArray[result]));
+            System.out.println(hexLetters);
             finalValue = finalValue + result * (int) Math.pow(10, decrementNum);
             inputNum = inputNum % ((int) Math.pow(endBaseNum, decrementNum));
             decrementNum--;
         }
-
+        if (endBaseNum == 16)
+        	finalHexLetters = hexLetters;
         return finalValue;
     }
 
 
     public int toBaseTen (Editable inputValue, int startBaseNum, int endBaseNum, int inputLength){
-        int incrementNum = 0, decrementNum = inputLength, total = 0;
+          int incrementNum = 0, decrementNum = inputLength - 1, total = 0;
+        int tempNum = 0;
+        String [] lettersArray = {"0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "A", "B", "C", "D", "E", "F"};
+        if (startBaseNum == 16)
+        for (int x = 0; x < inputLength; x++){
+        tempNum= Arrays.asList(lettersArray).indexOf(inputValue.substring(x, x + 1));
+        total =  total + (tempNum * (int) Math.pow(startBaseNum, decrementNum));
+        decrementNum--;
+        }
+        else
         while(incrementNum != inputLength){
-            total =  total + Integer.parseInt(inputValue.subSequence(decrementNum - 1, decrementNum).toString()) * (int) Math.pow(startBaseNum, incrementNum);
+        	total =  total + Integer.parseInt(inputValue.subSequence(decrementNum - 1, decrementNum).toString()) * (int) Math.pow(startBaseNum, incrementNum);
             incrementNum++;
             decrementNum--;}
         return total;
 
     }
+
+static String finalHexLetters = null;
 
 }
